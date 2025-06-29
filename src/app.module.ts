8dade7ce957d13fdca114dfeacea5186323f56c2
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-06-28 23:04:24 -0300

    iniciando projeto e bd

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'seu_usuario',
+      password: 'sua_senha',
+      database: 'ticketeria',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
