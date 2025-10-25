0830e7c9db41f23b40ad29546f9519abd07c16a5
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-10-25 14:02:10 -0300

    Feat: ajustando arquivos de configuração

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import 'dotenv/config';
+
 
 @Module({
   imports: [
